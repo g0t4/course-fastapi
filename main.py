ccfeb0035825eb7a44926ab722cbfcc9f869e6a1
Wes Higbee <wes.mcclure@gmail.com>
2025-05-30 15:10:12 -0500

    async vs sync

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ app = FastAPI()
 async def root():
     return {"message": "Hello There Two"}
 
+
+# async vs sync
+
+@app.get("/fastapi")
+async def hello_async():
+    return "Hello FastAPI ASYNC"
+
+@app.get("/fastapi_sync")
+def hello_sync():
+    return "Hello FastAPI SYNC"
