ccd283ba13c035beb45a63345e3285b58e7a5d23
Wes Higbee <wes.mcclure@gmail.com>
2025-06-05 02:58:57 -0500

    ws example

--- ws/main.py
+++ ws/main.py
@@ -3,6 +3,19 @@ from fastapi import FastAPI, WebSocket, WebSocketDisconnect, WebSocketException
 app = FastAPI()
 
 
+@app.websocket("/")
+async def ws1(ws: WebSocket):
+    await ws.accept()
+    await ws.send_text("welcome")
+
+    async for msg in ws.iter_text():
+        await ws.send_text(f"echo: {msg}")
+        if msg == ":quit":
+            break
+
+    await ws.close()
+    print("done")
+
 
 
 
